fd71de8b0c9391bdc55296c47824afff23d1d50b
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-23 00:57:41 -0500

    feat: move index.scss to Router in order to apply as global styles instead only entry-client.tsx

--- mercadolibre-vite-app/src/Router.tsx
+++ mercadolibre-vite-app/src/Router.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import './index.scss';
 // Import pages
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
--- mercadolibre-vite-app/src/entry-client.tsx
+++ mercadolibre-vite-app/src/entry-client.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import Router from './Router.tsx';
-import './index.css';
 
 ReactDOM.hydrateRoot(
   document.getElementById('app') as HTMLElement,
