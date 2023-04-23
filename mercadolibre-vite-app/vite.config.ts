7cab7cd50b3be5f07fdddaf983aa7824f7b0b4a8
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-23 16:41:08 -0500

    fix: update some dependencies and configs to use alias for paths

--- mercadolibre-vite-app/vite.config.ts
+++ mercadolibre-vite-app/vite.config.ts
@@ -1,11 +1,25 @@
 /// <reference types="vitest" />
 /// <reference types="Vite/client" />
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@components': path.resolve(__dirname, './src/components/'),
+      '@pages': path.resolve(__dirname, './src/pages/'),
+      '@hooks': path.resolve(__dirname, './src/hooks/'),
+      '@contexts': path.resolve(__dirname, './src/contexts/'),
+      '@constants': path.resolve(__dirname, './src/common/constants/'),
+      '@styles': path.resolve(__dirname, './src/common/styles/'),
+      '@utils': path.resolve(__dirname, './src/common/utils/'),
+      '@types': path.resolve(__dirname, './src/@types/'),
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
