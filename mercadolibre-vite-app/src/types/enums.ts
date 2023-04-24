9dd9bfb6443f3afcbc0b53367a5c1200fb271e6d
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-24 12:50:37 -0500

    feat: test enums from typescript for components

--- mercadolibre-vite-app/src/types/enums.ts
+++ mercadolibre-vite-app/src/types/enums.ts
@@ -1,3 +1,11 @@
-enum InputMaxLength {
+export enum InputMaxLength {
   MAX_LENGTH = 50,
+}
+
+export enum DetailConstants {
+  conditionNew = 'new',
+  sold = 'vendidos',
+  newText = 'Nuevo -',
+  buy = 'Comprar',
+  descTitle = 'Descripción del producto',
 }
